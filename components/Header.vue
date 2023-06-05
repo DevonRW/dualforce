3a2a38e525d2efa52fd56ded38d19bfdcca6ee76
Devon Rathie-Wright <devon.w@gmail.com>
2023-06-05 12:10:28 -0600

    basic theme for header (#4)
    may add in dark/light mode toggle later

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,8 @@
 <template>
   <header>
-    It's the Header
+    <div className="flex flex-no-wrap
+      px-4 lg:px-6 py-4
+      text-white bg-black
+      border-b-2 border-blue-600 border-opacity-70">DC DualForce Card Demo</div>
   </header>
 </template>
