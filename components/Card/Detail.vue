d3a3164e88d886889bbf13d7b7f6f3b0f69dccf8
Devon Rathie-Wright <devon.w@gmail.com>
2023-06-05 17:58:06 -0600

    Theme card details (#7)
    * general layout for cards added
    
    added layout via flexbox
    set it up so that it takes up how much space is given. The goal is to hopefully require less work for responsive layout
    The image, name, faction, and description are displaying
    Faction has it's colour matching the faction colour on the card
    
    * don't render the wrapping div if card.effect is empty
    
    * added a nice mouseover effect onto the image
    
    still need to figure out displaying the remaining data thought.
    will get Search & Filter going and then figure out remaining data

--- components/Card/Detail.vue
+++ components/Card/Detail.vue
@@ -1,18 +1,19 @@
 <template>
-  <article className="border-b-2">
-    <ul>
-      <li>Name: {{ card.name }}</li>
-      <li>Slug: {{ card.slug }}</li>
-      <li>Card Type: {{ card.card_type }}</li>
-      <li>Subname: {{ card.subname }}</li>
-      <li>Attack: {{ card.attack }}</li>
-      <li>Defense: {{ card.defense }}</li>
-      <li>Cost Grade: {{ card.cost_grade }}</li>
-      <li>Cost Value: {{ card.cost_value }}</li>
-      <li>Effect: {{ card.effect }}</li>
-      <li>Card Set: {{ card.card_set }}</li>
-      <li><img :src="`${getImage(card)}`" loading="lazy" alt="" /></li>
-    </ul>
+  <article>
+   <div
+      :data-type="card.card_type"
+      :data-card_set="card.card_set"
+      :data-subname="card.subname"
+      :data-attack="card.attack"
+      :data-defense="card.defense"
+      :data-cost_grade="card.cost_grade"
+      :data-cost_value="card.cost_value"
+    >
+      <img :src="`${getImage(card.slug)}`" alt="" class="hover:scale-110  transition-transform hover:drop-shadow-[0_10px_10px_rgb(253,213,0)]" />
+      <h2 class="font-bold mb-2">{{ card.name }}</h2>
+      <div v-if="card.faction" :class="card.faction">{{ card.faction }}</div>
+      <div v-if="card.effect" v-html="card.effect" class="text-sm"></div>
+    </div>
   </article>
 </template>
 
@@ -24,10 +25,33 @@
         required: true
       }
     },
+    data() {
+      return {
+        tyranny: 'purple'
+      }
+    },
     methods: {
-      getImage(card : Object) : string {
-        return `https://static.dcdualforce.gg/cards/${card.slug}.png`
+      getImage(slug : string) : string {
+        return `https://static.dcdualforce.gg/cards/${slug}.png`
       }
     }
   }
 </script>
+
+<style lang="postcss">
+  .tyranny {
+    @apply bg-purple-500 rounded-lg py-1 text-sm mb-2 capitalize;
+  }
+  .might {
+    @apply bg-red-500 rounded-lg py-1 text-sm mb-2 capitalize;
+  }
+  .tactics {
+    @apply bg-blue-500 rounded-lg py-1 text-sm mb-2 capitalize;
+  }
+  .anarchy {
+    @apply bg-yellow-500 rounded-lg py-1 text-sm mb-2 capitalize;
+  }
+  .energy {
+    @apply bg-green-500 rounded-lg py-1 text-sm mb-2 capitalize;
+  }
+</style>
