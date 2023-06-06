99d30f25bf6af535acb1b42941d047c842a78ef6
Devon Rathie-Wright <devon.w@gmail.com>
2023-06-06 14:03:23 -0600

    display remaining card details on hover (#14)

--- components/Card/Detail.vue
+++ components/Card/Detail.vue
@@ -1,15 +1,32 @@
 <template>
-  <article>
+  <article
+    v-on:mouseover="setDisplay(true)"
+    v-on:mouseout="setDisplay(false)"
+    >
     <img :src="`${getImage(card.slug)}`" alt="" class="hover:scale-110  transition-transform hover:drop-shadow-[0_10px_10px_rgb(253,213,0)]" />
-    <h2 class="font-bold mb-2">{{ card.name }}</h2>
-    <div v-if="card.faction" :class="card.faction">{{ card.faction }}</div>
-    <div v-if="card.effect" v-html="card.effect" class="text-sm"></div>
+    <section v-show="!displayDetails">
+      <h2 class="font-bold mb-2">{{ card.name }}</h2>
+      <div v-if="card.faction" :class="card.faction">{{ card.faction }}</div>
+      <div v-if="card.effect" v-html="card.effect" class="text-sm"></div>
+    </section>
+    <section v-show="displayDetails">
+      <h2 class="font-bold mb-2">{{ card.name }}</h2>
+      <div class="capitalize">Cost: {{ card.cost_grade }}</div>
+      <div class="capitalize">Rarity: {{ card.rarity }}</div>
+      <div>Power: {{ card.attack }}</div>
+      <div>Health: {{ card.defense }}</div>
+    </section>
   </article>
 </template>
 
 <script lang="ts">
 
   export default {
+    data() {
+      return {
+        displayDetails: false
+      }
+    },
     props: {
       card: {
         type: Object,
@@ -19,6 +36,9 @@
     methods: {
       getImage(slug : string) : string {
         return `https://static.dcdualforce.gg/cards/${slug}.png`
+      },
+      setDisplay(val: boolean) {
+        this.displayDetails = val
       }
     }
   }
