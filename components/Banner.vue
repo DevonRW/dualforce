86d313af0081e6b9e55b9052746464e452d88e83
Devon Rathie-Wright <devon.w@gmail.com>
2023-06-06 13:16:02 -0600

    added blurb about the site (#12)

---
+++ components/Banner.vue
@@ -0,0 +1,6 @@
+<template>
+  <section class="text-center text-white py-4">
+    <h1 class="text-3xl">DC DualForce Card DB</h1>
+    <p>All the DualForce cards in on place</p>
+  </section>
+</template>
